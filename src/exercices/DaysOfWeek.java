360081df51570fbcb628ad5555f3c12231a6c46b
jorgevft <jvillafuerte@unikino.mx>
2017-10-05 19:14:00 -0700

    Días de la semana

--- src/exercices/DaysOfWeek.java
+++ src/exercices/DaysOfWeek.java
@@ -11,12 +11,35 @@ public class DaysOfWeek {
 
         Scanner scann = new Scanner( System.in );
 
-        System.out.println("Number of the day");
+        System.out.println("ingresa numero de día de la semana");
 
         String day = scann.next();
 
         switch ( day ){
             case "1":
+                System.out.print("lunes");
+                break;
+            case "2":
+                System.out.print("martes");
+                break;
+            case "3":
+                System.out.print("miercoles");
+                break;
+            case "4":
+                System.out.print("jueves");
+                break;
+            case "5":
+                System.out.print("viernes");
+                break;
+            case "6":
+                System.out.print("sabado");
+                break;
+            case "7":
+                System.out.print("domingo");
+                break;
+            default:
+                System.out.print("no es un día de la semana");
+
 
         }
 
