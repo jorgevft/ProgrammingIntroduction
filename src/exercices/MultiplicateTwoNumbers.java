b80583be531ff91b717092ad039d78e759df80a0
jorgevft <jvillafuerte@unikino.mx>
2017-10-05 19:44:00 -0700

    Multiplicar dos numeros

--- src/exercices/MultiplicateTwoNumbers.java
+++ src/exercices/MultiplicateTwoNumbers.java
@@ -10,14 +10,21 @@ public class MultiplicateTwoNumbers {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        System.out.print("Input first number: ");
+        System.out.println("dame primer número: ");
         int num1 = in.nextInt();
 
         //Todo: get the second number
+        System.out.println("dame segundo número: ");
+        int num2 = in.nextInt();
 
         //multiply 2 numbers
+        int total = num1 * num2;
+
 
         //print the result
+        System.out.println("el resultado es ");
+        System.out.print(total);
+
 
     }
 
